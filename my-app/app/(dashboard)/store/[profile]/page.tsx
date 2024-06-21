9448f688be3e8b9c5265c1d65e4f0743e7485ef4
chrisIyer <hellohotbox420@gmail.com>
2024-06-21 16:11:45 -0400

    minor changes

--- my-app/app/(dashboard)/store/[profile]/page.tsx
+++ my-app/app/(dashboard)/store/[profile]/page.tsx
@@ -1,10 +1,10 @@
-// 'use client'
-const ProfilePage = ({params}) => {
-  return (
-    <div>
-      <h1>Tell me a little about yourself {params.id}</h1>
-    </div>
-  )
-}
-
-export default ProfilePage
+'use client'
+const PName = ({params}) => {
+     return (
+       <div>
+         <h1>Start by documenting your P: {params.profile}</h1>
+       </div>
+     )
+   }
+   
+   export default PName
